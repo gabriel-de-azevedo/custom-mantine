6425e45625d74f18dd17a784c9cec1bafa33d2a3
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-29 11:32:37 -0300

    feat: add AppShell layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { Center, Container } from '@mantine/core'
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import RichText from './components/RichText'
 
 function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-      <Center w="100vw">
-          <RichText />
-      </Center>
-  )
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        />
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>
+        <RichText/>
+      </AppShell.Main>
+    </AppShell>
+  );
 }
 
 export default App
