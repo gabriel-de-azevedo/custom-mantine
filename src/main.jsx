10685ee0437923d911db0a092423d8b9d302be79
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-29 11:19:11 -0300

    feat: add mantine rich text editor

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@mantine/core/styles.css';
+import '@mantine/tiptap/styles.css';
 import App from './App.jsx'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
